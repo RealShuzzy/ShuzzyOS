2a99803851b175267dd55f7046f321227b7a1b45
RealShuzzy <samirvonhansen2016@gmail.com>
2025-08-13 08:22:58 +0200

    auto monitor setup

--- assets/hyprland_setup.sh
+++ assets/hyprland_setup.sh
@@ -1,25 +1,25 @@
-#swww wallpaper,get monitor config and maybe before get grum and sddm I guess
-LIBGL_ALWAYS_SOFTWARE=1 kitty
-echo "Setting up hyprland"
-
+#!/bin/bash
 swww img ~/pictures/wallpaper/wallpaper.png
 
-#getmonitors
-monitors=""
+monitors=()
 
 while IFS= read -r line; do
-    if [[ $line =~ ^Monitor[[:space:]]+([A-Za-z0-9+]) ]]; then
+    if [[ $line =~ ^Monitor[[:space:]]+([A-Za-z0-9\+\-]+) ]]; then
         name="${BASH_REMATCH[1]}"
     elif [[ $line =~ ^[[:space:]]*([0-9]+x[0-9]+)@ ]]; then
         size="${BASH_REMATCH[1]}"
     elif [[ $line =~ ^[[:space:]]*Position:[[:space:]]*([0-9]+x[0-9]+) ]]; then
         offset="${BASH_REMATCH[1]}"
-        monitors+="monitor = ${name},${size},${offset},1.0"
+        monitors+=("monitor = ${name},${size},${offset},1.0")
     fi
 done < <(hyprctl monitors)
-#
+
+# Write to the config file
 for monitor in "${monitors[@]}"; do
-    sudo echo $monitor >> ~/.config/hypr/test.conf
+    echo "$monitor" >> ~/.config/hypr/test.conf
 done
+
+LIBGL_ALWAYS_SOFTWARE=1 kitty
+
 
 rsync ~/.ShuzzyOS/assets/hyprland.conf ~/.config/hypr/hyprland.conf
