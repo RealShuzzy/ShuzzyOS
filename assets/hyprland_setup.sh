f4d2558ab9e7080a49f8ca628ef3990fbf03306d
RealShuzzy <samirvonhansen2016@gmail.com>
2025-08-13 10:39:47 +0200

    added kitty and fixed monitor

--- assets/hyprland_setup.sh
+++ assets/hyprland_setup.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 swww img ~/pictures/wallpaper/wallpaper.png
 
-monitors=()
-
-while IFS= read -r line; do
-    if [[ $line =~ ^Monitor[[:space:]]+([A-Za-z0-9\+\-]+) ]]; then
-        name="${BASH_REMATCH[1]}"
-    elif [[ $line =~ ^[[:space:]]*([0-9]+x[0-9]+)@ ]]; then
-        size="${BASH_REMATCH[1]}"
-    elif [[ $line =~ ^[[:space:]]*Position:[[:space:]]*([0-9]+x[0-9]+) ]]; then
-        offset="${BASH_REMATCH[1]}"
-        monitors+=("monitor = ${name},${size},${offset},1.0")
-    fi
-done < <(hyprctl monitors)
-
-# Write to the config file
-for monitor in "${monitors[@]}"; do
-    echo "$monitor" >> ~/.config/hypr/test.conf
+for mon in $(hyprctl monitors | grep "Monitor" | awk '{print $2}'); do
+    echo "monitor = $mon,preferred,auto,1.0" >> ~/.config/hypr/test.conf
 done
-
-LIBGL_ALWAYS_SOFTWARE=1 kitty
-
 
 rsync ~/.ShuzzyOS/assets/hyprland.conf ~/.config/hypr/hyprland.conf
