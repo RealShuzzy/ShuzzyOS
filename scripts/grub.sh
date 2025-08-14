d49dfc10097583f1091fb0b84caa3340b191a12b
RealShuzzy <samirvonhansen2016@gmail.com>
2025-08-14 10:58:26 +0200

    fixed grub again

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -1,4 +1,4 @@
-$EFI_DIR = "/boot"
+$efi_dir = "/boot"
 sudo pacman -S --needed --noconfirm grub efibootmgr os-prober > /dev/null
-sudo grub-install --target=x86_64-efi --efi-directory="$EFI_DIR" --bootloader-id=GRUB
+sudo grub-install --target=x86_64-efi --efi-directory="$efi_dir" --bootloader-id=GRUB
 sudo grub-mkconfig -o /boot/grub/grub.cfg
