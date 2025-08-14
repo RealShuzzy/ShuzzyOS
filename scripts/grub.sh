1c8aa46028b5c64ee1a2d084d80792199b74435b
RealShuzzy <samirvonhansen2016@gmail.com>
2025-08-14 11:17:14 +0200

    and again... and again...

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -1,4 +1,3 @@
-$efi_dir = "/boot"
 sudo pacman -S --needed --noconfirm grub efibootmgr os-prober > /dev/null
-sudo grub-install --target=x86_64-efi --efi-directory=$efi_dir --bootloader-id=GRUB
+sudo grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 sudo grub-mkconfig -o /boot/grub/grub.cfg
