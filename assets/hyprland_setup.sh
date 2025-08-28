b546e4b7e688a5eb8e04ee0e919ee78f1d87fd1d
RealShuzzy <samirvonhansen2016@gmail.com>
2025-08-28 10:44:41 +0200

    path migration done

--- assets/hyprland_setup.sh
+++ assets/hyprland_setup.sh
@@ -3,4 +3,4 @@ for mon in $(hyprctl monitors | grep "Monitor" | awk '{print $2}'); do
     echo "monitor = $mon,preferred,auto,1.0" >> ~/.config/hypr/monitor.conf
 done
 
-rsync ~/.ShuzzyOS/assets/hyprland.conf ~/.config/hypr/hyprland.conf
+rsync ~/git/ShuzzyOS/assets/hyprland.conf ~/.config/hypr/hyprland.conf
--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -4,3 +4,4 @@ sudo chsh -s /usr/bin/zsh $USER
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/git/powerlevel10k
 echo 'source ~/git/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 echo '[[ ! -f ~/git/ShuzzyOS/assets/.p10k.zsh ]] || source ~/git/ShuzzyOS/assets/.p10k.zsh' >> ~/.zshrc
+rm -rf ~/.bash*
