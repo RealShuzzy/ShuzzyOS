021fd2d114eb1674aaeed48ff3a65f716f6918d7
RealShuzzy <samirvonhansen2016@gmail.com>
2025-09-15 14:14:56 +0200

    added dracula grub

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -1,3 +1,8 @@
 sudo pacman -S --needed --noconfirm efibootmgr os-prober
 sudo grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+
+git clone https://github.com/dracula/grub.git ~/git/dracula-grub
+sudo rsync -r ~/git/dracula-grub/dracula /boot/grub/themes/
+sudo rsync ~/git/ShuzzyOS/assets/grub /etc/default/grub
+
 sudo grub-mkconfig -o /boot/grub/grub.cfg
